c183e43785c878b946e090566bfb7d6e490e4940
Fritz Otlinghaus <fritz@otlinghaus.it>
2015-08-30 20:55:51 +0200

    created charity database model

---
+++ charity/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Charity(models.Model):
+    classification = models.CharField(max_length=200)7
+    name = models.CharField(max_length=300)
+    number_of_direct_beneficiaries = models.IntegerField()
+    number_of_indirect_beneficiaries = models.IntergerField()
+    annual_cost = models.FloatField()
+    cost_per_direct_beneficiary = models.FloatField()
+    cost_per_indirect_beneficiary = models.FloatField()
+    comments = models.TextField()
+    source = models.TextField()
